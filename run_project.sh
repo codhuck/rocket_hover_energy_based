fec16985c2e695051133ab71b79cea662021b4d5
derevoAM <derevianchenko.ma@phystech.edu>
2026-05-13 20:29:36 +0300

    fix shell scripts and requirements

--- run_project.sh
+++ run_project.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python -m src.main --config configs/default.yaml --output-root .
+python -m src.main --config configs/backstepping.yaml --output-root outputs/backstepping --animate
+python -m src.main --config configs/pid_cascade.yaml --output-root outputs/pid_cascade --animate
